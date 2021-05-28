788c93cf8ed2019c20199f0983695b885b22d411
Nandita Vora <Nandita.Vora@in.niit.com>
2021-05-29 01:07:16 +0530

    pattern problem statement modified

--- pattern.js
+++ pattern.js
@@ -1,2 +1,16 @@
+/*
+Generate the below pattern
+
+        *
+       * *
+      * * *
+     * * * *
+    * * * * *
+    
+
+No. of rows is determined by the input provided to the function
+*/
+
+
 function checkPattern() {
 }
